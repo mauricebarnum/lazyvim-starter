c9d93af6a28fe38cead6d1b8d19c1c573bb2bc06
mauricebarnum <maurice.barnum@gmail.com>
2024-06-11 22:22:33 -0700

    local options and keymaps

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+local opt = vim.opt
+
+opt.autowrite = false
+opt.background = "light"
+opt.clipboard = ""
+opt.ignorecase = false
+opt.inccommand = "split"
+opt.number = false
+opt.relativenumber = false
+opt.shiftwidth = 4
