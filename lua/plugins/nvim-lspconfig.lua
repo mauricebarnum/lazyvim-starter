4fa178176fbb94ab6948cc24c1dd16c90fa86d34
mauricebarnum <maurice.barnum@gmail.com>
2024-08-09 15:39:51 -0700

    more tweaks, experiments with modules

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,16 +1,47 @@
-if true then
-  return {}
-end
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    on_attach = function(client, bufnr)
-      local on_attach = require("lazyvim.util").lsp.on_attach
-      on_attach(client, bufnr)
-
-      vim.api.nvim_buf_del_keymap(bufnr, "i", "<C-k>")
-      vim.api.nvim_buf_set_keymap(bufnr, "i", "<C-k>", "<C-o>D", { noremap = true, desc = "Delete to end of line" })
-      vim.api.nvim_buf_set_keymap(bufnr, "i", "<C-S-k>", "", { callback = vim.lsp.buf.signature_help })
-    end,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      --    return {
+      --      --    on_attach = function(client, bufnr)
+      --      --      local on_attach = require("lazyvim.util").lsp.on_attach
+      --      --      on_attach(client, bufnr)
+      --      --
+      --      --      vim.api.nvim_buf_del_keymap(bufnr, "i", "<C-k>")
+      --      --      vim.api.nvim_buf_set_keymap(bufnr, "i", "<C-k>", "<C-o>D", { noremap = true, desc = "Delete to end of line" })
+      --      --      vim.api.nvim_buf_set_keymap(bufnr, "i", "<C-S-k>", "", { callback = vim.lsp.buf.signature_help })
+      --      --    end,
+      --      servers = {
+      --        gopls = {
+      --          codelenses = {
+      --            gc_details = true,
+      --          },
+      --          hints = {
+      --            parameterNames = false,
+      --          },
+      --        },
+      --      },
+      --    }
+      servers = {
+        gopls = {
+          hints = {
+            parameterNames = false,
+          },
+        },
+      },
+    },
   },
+  --  {
+  --    "neovim/nvim-lspconfig",
+  --    opts = function()
+  --      -- local keys = require("lazyvim.plugins.lsp.keymaps").get()
+  --      -- keys[#keys + 1] = { "i", "<C-k>", "C-o>D", { noremap = true } }
+  --      -- keys[#keys + 1] = { "i", "<C-S-k>", "", { callback = vim.lsp.buf.signature_help } }
+  --      -- keys[#keys + 1] = { "<C-S-k>", "", { callback = vim.lsp.buf.signature_help } }
+  --      --            -      vim.api.nvim_buf_del_keymap(bufnr, "i", "<C-k>")
+  --      ---      vim.api.nvim_buf_set_keymap(bufnr, "i", "<C-k>", "<C-o>D", { noremap = true, desc = "Delete to end of line" })
+  --      ---      vim.api.nvim_buf_set_keymap(bufnr, "i", "<C-S-k>", "", { callback = vim.lsp.buf.signature_help })
+  --      --
+  --    end,
+  --  },
 }
