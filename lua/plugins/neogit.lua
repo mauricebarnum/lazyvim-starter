7ac64dd28fdafcdfdc21489c6de34353acd3144e
mauricebarnum <maurice.barnum@gmail.com>
2024-08-10 12:29:12 -0700

    add neogit plugin

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true,
+    keys = {
+      { "<leader>G", "<cmd>Neogit<cr>", desc = "Neogit" },
+    },
+  },
+}
