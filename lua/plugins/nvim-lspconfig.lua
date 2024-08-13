0829c43c8a05206aec2328421e2afa28d591f100
mauricebarnum <maurice.barnum@gmail.com>
2024-08-12 21:07:50 -0700

    Revert "enable rayx-go plugin"
    This reverts commit 1d536a92e4a63627582c7daac61a4520d823e673.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,17 +1,11 @@
-if true then
-  return {}
-end
-
 return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
       gopls = {
-        settings = {
-          hints = {
-            parameterNames = false,
-            functionTypeParameters = false,
-          },
+        hints = {
+          parameterNames = false,
+          functionTypeParameters = false,
         },
       },
     },
