3ffceeb866b9cd18d97902636f76c8caefcaf561
mauricebarnum <maurice.barnum@gmail.com>
2024-11-13 18:34:19 -0800

    disable noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,13 +1,13 @@
 -- ~/.config/nvim/lua/plugins/noice.lua
 return {
-  "folke/noice.nvim",
-  opts = {
-    cmdline = {
-      enabled = true,
-      view = "cmdline",
-    },
-    messages = {
-      enabled = false,
-    },
-  },
+  -- "folke/noice.nvim",
+  -- opts = {
+  --   cmdline = {
+  --     enabled = true,
+  --     view = "cmdline",
+  --   },
+  --   messages = {
+  --     enabled = false,
+  --   },
+  -- },
 }
