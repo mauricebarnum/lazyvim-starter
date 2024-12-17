07a7a8a08cb9cf1f6c148ddf4cfea9700c8a4e54
mauricebarnum <maurice.barnum@gmail.com>
2024-12-16 19:53:42 -0800

    settings tweaks

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,18 +1,22 @@
 return {
   {
-    "akinsho/bufferline.nvim",
+    "echasnovski/mini.pairs",
     enabled = false,
   },
+  {
+    "ggandor/leap.nvim",
+    enabled = false,
+  },
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   enabled = false,
+  -- },
   {
     "nvim-lualine/lualine.nvim",
     enabled = false,
   },
   {
     "folke/noice.nvim",
-    enabled = true,
-  },
-  {
-    "folke/flash.nvim",
     enabled = false,
   },
 }
